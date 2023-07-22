4ad1d323598ffe362e8d7ca09716e7ee66343fe0
OlgaBilynska <olgabilinskaya@gmail.com>
2023-07-22 08:11:19 +0300

    changing components

--- src/components/ContactForm/Form.jsx
+++ src/components/ContactForm/Form.jsx
@@ -1,58 +1,33 @@
-import React, { Component } from 'react';
+// import React from 'react';
 import { Formik, Form, Field } from 'formik';
-import { nanoid } from 'nanoid';
-import styled from '@emotion/styled';
+// import { nanoid } from 'nanoid';
+// import styled from '@emotion/styled';
 
 const initialValues = {
-  name: '',
-  number: '',
+  name: 'Olga',
+  number: '1233',
 };
 
-const Input = styled(Field)`
-  font-size: 30px;
-`;
+// const Input = styled(Field)`
+//   font-size: 30px;
+// `;
 
-const Form = () => {
-  //   state = {
-  //     name: '',
-  //     number: '',
-  //   };
-
-  //   nameInputId = nanoid();
-  //   numberInputId = nanoid();
-
-  //   handleChange = e => {
-  //     const { name, value } = e.currentTarget;
-
-  //     this.setState({ [name]: value });
-  //   };
-
-  //   handleSubmit = e => {
-  //     e.preventDefault();
-  //     // const contactID = nanoid();
-  //     // const contact = { id: contactID, ...this.state };
-  //     this.props.onSubmit(this.state);
-  //     // this.props.onContactExist(this.state.name);
-  //     this.reset();
-  //   };
-
-  //   reset = () => {
-  //     this.setState({ name: '', number: '' });
-  //   };
-
-  //   render() {
-  //     return (
+export const ContactForm = () => {
+  const handleSubmit = (values, actions) => {
+    console.log('values', values);
+    console.log('actions', actions);
+  };
 
   return (
-    <Formik initialValues={initialValues}>
-      <Form onSubmit={this.handleSubmit}>
-        <label htmlFor={this.nameInputId}>
-          Name
-          <Input
-            value={this.state.name}
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form autoComplete="on">
+        <label htmlFor="name">
+          Names
+          <Field
+            // value={this.state.name}
             name="name"
-            id={this.nameInputId}
-            onChange={this.handleChange}
+            // id={this.nameInputId}
+            // onChange={this.handleChange}
             type="text"
             //   pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -60,13 +35,13 @@ const Form = () => {
           />
         </label>
         <br />
-        <label htmlFor={this.numberInputId}>
+        <label htmlFor="number">
           Number
-          <Input
-            value={this.state.number}
+          <Field
+            // value={this.state.number}
             name="number"
-            id={this.numberInputId}
-            onChange={this.handleChange}
+            // id={this.numberInputId}
+            // onChange={this.handleChange}
             type="tel"
             //   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
@@ -80,4 +55,32 @@ const Form = () => {
   );
 };
 
-export default Form;
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   nameInputId = nanoid();
+//   numberInputId = nanoid();
+
+//   handleChange = e => {
+//     const { name, value } = e.currentTarget;
+
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     // const contactID = nanoid();
+//     // const contact = { id: contactID, ...this.state };
+//     this.props.onSubmit(this.state);
+//     // this.props.onContactExist(this.state.name);
+//     this.reset();
+//   };
+
+//   reset = () => {
+//     this.setState({ name: '', number: '' });
+//   };
+
+//   render() {
+//     return (
