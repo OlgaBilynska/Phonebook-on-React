89b670fe48b81cec2dadcc79f269a1ebd40e8df1
OlgaBilynska <olgabilinskaya@gmail.com>
2023-07-26 08:32:08 +0300

    fixing styles

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   height: 100%;
-  padding: 20px 450px;
+  padding: 20px 50px;
   background-image: linear-gradient(
     to bottom,
     var(--background),
